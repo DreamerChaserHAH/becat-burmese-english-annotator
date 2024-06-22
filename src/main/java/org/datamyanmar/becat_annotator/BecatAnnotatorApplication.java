faee5603fe891fef8917a921a70410bbdf76dea2
Victor Mak <contact@htetaung.com>
2024-06-22 12:04:09 +0800

    feat: backend project setup

---
+++ src/main/java/org/datamyanmar/becat_annotator/BecatAnnotatorApplication.java
@@ -0,0 +1,13 @@
+package org.datamyanmar.becat_annotator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecatAnnotatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecatAnnotatorApplication.class, args);
+	}
+
+}
